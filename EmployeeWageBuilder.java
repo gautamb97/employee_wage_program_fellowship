28539520eb7b24adafb5b3af27cd94d58b45c3b3
gautamb97 <www.gautam971997@gmail.com>
2020-12-15 08:44:19 +0530

    Modified:calculated_daily_employee_wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,14 +3,18 @@ package com.company;
 public class EmployeeWageBuilder {
     public static void main(String args[]){
         System.out.println("Welcome to employee wage program");
-        //Constants
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
         //Computation
-        double employeeCheck = Math.floor(Math.random() * 10) % 2;
-        if(employeeCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
-        }else{
-            System.out.println("Employee is Absent");
-        }
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
