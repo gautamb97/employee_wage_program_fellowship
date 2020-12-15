ffaff51314070fc049fd96114e3783f8247a16c7
gautamb97 <www.gautam971997@gmail.com>
2020-12-15 08:37:07 +0530

    Added:checking_employee_pressence

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,7 +1,16 @@
 package com.company;
 
 public class EmployeeWageBuilder {
-    public static void main(String[] args){
+    public static void main(String args[]){
         System.out.println("Welcome to employee wage program");
+        //Constants
+        int IS_FULL_TIME = 1;
+        //Computation
+        double employeeCheck = Math.floor(Math.random() * 10) % 2;
+        if(employeeCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
