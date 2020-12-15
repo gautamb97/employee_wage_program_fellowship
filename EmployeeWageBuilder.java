7e6f67808fb5a47475be9539c1fef0d09adf10e3
gautamb97 <www.gautam971997@gmail.com>
2020-12-15 09:28:27 +0530

    Modified:wages_reaches_total_work_hours_or_working_days

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,14 +5,18 @@ public class EmployeeWageBuilder {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 20;
 
     public static void main(String args[]){
         System.out.println("Welcome to employee wage program");
 
         //Variables
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        int totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         //Computation
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+            //Computation
+            int empHrs = 0;
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -24,7 +28,8 @@ public class EmployeeWageBuilder {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpHrs +=empHrs;
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
             totalEmpWage += empWage;
             System.out.println("Emp Wage: " + empWage);
         }
