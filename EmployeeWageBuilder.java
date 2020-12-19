992f773fe43f27756b35829a0630ea395e286c9f
gautamb97 <www.gautam971997@gmail.com>
2020-12-19 11:04:26 +0530

    Added:compute_employee_wage_for_multiple_companies

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,17 +3,12 @@ package com.company;
 public class EmployeeWageBuilder {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    public static final int MAX_HRS_IN_MONTH = 20;
 
-    public static int computeEmployeeWage(){
+    public static int computeEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
         //Variables
-        int totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         //Computation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
-            //Computation
-            int empHrs = 0;
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -26,16 +21,17 @@ public class EmployeeWageBuilder {
                 default:
                     empHrs = 0;
             }
-            totalEmpHrs +=empHrs;
+            totalEmpHrs += empHrs;
             System.out.println("Days#: " + totalWorkingDays + " Emp Hr: " +empHrs);
         }
-        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for Company: " +company+" is:"+totalEmpWage);
         return totalEmpWage;
     }
 
     public static void main(String args[]){
         System.out.println("Welcome to employee wage program");
-        computeEmployeeWage();
+        computeEmployeeWage("DMart", 20, 2, 10);
+        computeEmployeeWage("Reliance", 10, 4, 20);
     }
 }
